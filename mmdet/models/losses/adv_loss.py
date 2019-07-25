b51237c61035befd1d1250a2a44b34af65909076
劉子铭 <zimingemail@163.com>
2019-07-25 17:17:34 +0800

    debug

--- mmdet/models/losses/adv_loss.py
+++ mmdet/models/losses/adv_loss.py
@@ -11,7 +11,7 @@ class AdversarialLoss(nn.Module):
 
     def __init__(self,
                  avg_factor=None,
-                 reduction='none',
+                 reduction='mean',
                  loss_weight=torch.FloatTensor([1]).cuda()):
         super(AdversarialLoss, self).__init__()
 
