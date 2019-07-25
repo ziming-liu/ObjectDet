1fb3a0e6715c3bffd754bb2a79cce2f175f4721c
劉子铭 <zimingemail@163.com>
2019-07-25 16:32:03 +0800

    adv loss is negtive number

--- mmdet/models/bbox_heads/bbox_head.py
+++ mmdet/models/bbox_heads/bbox_head.py
@@ -139,7 +139,7 @@ class BBoxHead(nn.Module):
         if adv_score is not None:# must be negtive loss
             losses['loss_adv'] = -1* self.loss_adv(
                 score=adv_score,
-                labels=size_labels,
+                label=size_labels,
                 weight=size_labels_weights)
             print("lossadv {}".format(losses['loss_adv']))
         return losses
