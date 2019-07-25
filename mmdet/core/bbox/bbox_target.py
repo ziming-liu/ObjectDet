1f8c6225e9d7aa31f4f704f4f75071f4b09b88b9
劉子铭 <zimingemail@163.com>
2019-07-25 16:43:46 +0800

    debug

--- mmdet/core/bbox/bbox_target.py
+++ mmdet/core/bbox/bbox_target.py
@@ -60,7 +60,7 @@ def bbox_target_single(pos_bboxes,
         bbox_targets[:num_pos, :] = pos_bbox_targets
         bbox_weights[:num_pos, :] = 1
         #print("gtboxszie {}".format(pos_gt_bboxes))
-        area = (pos_gt_bboxes[:,3] - pos_gt_bboxes[:,1])  * (pos_gt_bboxes[:,2]-pos_gt_bboxes[:,0]).view(-1)
+        area = (pos_gt_bboxes[:,3] - pos_gt_bboxes[:,1])  * (pos_gt_bboxes[:,2]-pos_gt_bboxes[:,0])
         #print("area {}".format(area))
         gt_size = torch.where(area<64*64,torch.full_like(area,0),torch.full_like(area,1))
         size_labels[:num_pos] = gt_size
