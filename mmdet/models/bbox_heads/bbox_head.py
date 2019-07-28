04e5cd996d00f28458099206e5b6c557621cfa93
劉子铭 <zimingemail@163.com>
2019-07-28 16:30:26 +0800

    fix single stage

--- mmdet/models/bbox_heads/bbox_head.py
+++ mmdet/models/bbox_heads/bbox_head.py
@@ -137,7 +137,7 @@ class BBoxHead(nn.Module):
                 avg_factor=bbox_targets.size(0),
                 reduction_override=reduction_override)
         if adv_score is not None:# must be negtive loss
-            losses['loss_adv'] = 1 /  self.loss_adv(
+            losses['loss_fc_adv'] = 1 /  self.loss_adv(
                 score=adv_score.view(-1).float(),
                 label=size_labels.float(),
                 weight=size_labels_weights.float())
