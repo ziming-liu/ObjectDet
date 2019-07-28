6a08345a1673d3d4e82f3f8fc3188e9a18855b82
劉子铭 <zimingemail@163.com>
2019-07-28 15:47:11 +0800

    fix single stage

--- mmdet/models/detectors/single_stage.py
+++ mmdet/models/detectors/single_stage.py
@@ -49,8 +49,8 @@ class SingleStageDetector(BaseDetector):
                       gt_labels,
                       gt_bboxes_ignore=None):
         x = self.extract_feat(img)
-        outs = self.bbox_head(x)
-        loss_inputs = outs + (gt_bboxes, gt_labels, img_metas, self.train_cfg)
+        cls_scores,adv_scores,bbox_pred = self.bbox_head(x)
+        loss_inputs = (cls_scores,adv_scores,bbox_pred) + (gt_bboxes, gt_labels, img_metas, self.train_cfg)
         losses = self.bbox_head.loss(
             *loss_inputs, gt_bboxes_ignore=gt_bboxes_ignore)
         return losses
