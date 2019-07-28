653568047c25ac5277a75b650a2b3ec4022c6794
劉子铭 <zimingemail@163.com>
2019-07-28 15:55:37 +0800

    fix single stage

--- mmdet/core/anchor/anchor_target.py
+++ mmdet/core/anchor/anchor_target.py
@@ -150,7 +150,7 @@ def anchor_target_single(flat_anchors,
         area = (pos_gt_bboxes[:, 3] - pos_gt_bboxes[:, 1]) * (pos_gt_bboxes[:, 2] - pos_gt_bboxes[:, 0])
         # print("area {}".format(area))
         gt_size = torch.where(area < 64 * 64 * 2, torch.full_like(area, 0), torch.full_like(area, 1))
-        size_labels[pos_inds] = gt_size
+        size_labels[pos_inds] = gt_size.long()
         # print("gt size {}".format(gt_size))
         size_labels_weights = label_weights
     if len(neg_inds) > 0:
@@ -164,9 +164,9 @@ def anchor_target_single(flat_anchors,
         bbox_targets = unmap(bbox_targets, num_total_anchors, inside_flags)
         bbox_weights = unmap(bbox_weights, num_total_anchors, inside_flags)
         size_labels = unmap(size_labels, num_total_anchors, inside_flags)
-        size_label_weights = unmap(size_label_weights, num_total_anchors, inside_flags)
+        size_labels_weights = unmap(size_labels_weights, num_total_anchors, inside_flags)
 
-    return (labels, label_weights,size_labels,size_label_weights, bbox_targets, bbox_weights, pos_inds,
+    return (labels, label_weights,size_labels,size_labels_weights, bbox_targets, bbox_weights, pos_inds,
             neg_inds)
 
 
