7e1a3b1a8fe89574c170dc66ce164301b4f52db9
劉子铭 <zimingemail@163.com>
2019-07-30 12:32:30 +0800

    config

--- configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_advloss.py
+++ configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_advloss.py
@@ -156,7 +156,7 @@ lr_config = dict(
 checkpoint_config = dict(interval=1)
 # yapf:disable
 log_config = dict(
-    interval=50,
+    interval=1,
     hooks=[
         dict(type='TextLoggerHook'),
         #dict(type='TensorboardLoggerHook')
