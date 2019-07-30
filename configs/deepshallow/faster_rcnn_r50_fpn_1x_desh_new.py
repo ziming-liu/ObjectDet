dc90541ceeb94afac60840e63a7312dbaf7e754e
劉子铭 <zimingemail@163.com>
2019-07-31 01:17:39 +0800

    config update

--- configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_new.py
+++ configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_new.py
@@ -3,12 +3,12 @@ model = dict(
     type='FasterRCNN',
     pretrained='modelzoo://resnet50',
     backbone=dict(
-        type='ResNet',
-        depth=50,
+        type='DSNet',
+        depth=('s3', 's1'),
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        style='pytorch'),
+        style='pytorch', with_cp=False),
     neck=dict(
         type='FPN',
         in_channels=[ 256, 512,1024,2048],
