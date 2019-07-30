fc1537c935287b8b78af73c83c512164acbe9c77
劉子铭 <zimingemail@163.com>
2019-07-30 19:22:58 +0800

    new DSnet

--- configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_new.py
+++ configs/deepshallow/faster_rcnn_r50_fpn_1x_desh_new.py
@@ -8,7 +8,7 @@ model = dict(
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        style='pytorch', with_cp=True),
+        style='pytorch', with_cp=False),
     neck=dict(
         type='FPN',
         in_channels=[64, 128, 256, 512, ],
