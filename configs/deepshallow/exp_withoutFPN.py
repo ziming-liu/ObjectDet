2e6b5b5b20b6e8db5b4b4c38b34f632fddf479c6
劉子铭 <zimingemail@163.com>
2019-07-31 10:17:08 +0800

    outlayer for desh

--- configs/deepshallow/exp_withoutFPN.py
+++ configs/deepshallow/exp_withoutFPN.py
@@ -138,13 +138,13 @@ data = dict(
         with_label=False,
         test_mode=True))
 # optimizer
-optimizer = dict(type='SGD', lr=0.001, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=20, norm_type=2))
 # learning policy
 lr_config = dict(
     policy='step',
     warmup='linear',
-    warmup_iters=50,
+    warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
 checkpoint_config = dict(interval=1)
