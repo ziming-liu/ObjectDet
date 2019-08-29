23ec94ad5e2e3071edaec2344122cdf4dee85f80
劉子铭 <zimingemail@163.com>
2019-08-29 09:16:32 +0800

    config

--- configs/shareresnet/cascade_rcnn_x101_64x4d_fpn_1x_shareresnet.py
+++ configs/shareresnet/cascade_rcnn_x101_64x4d_fpn_1x_shareresnet.py
@@ -189,7 +189,7 @@ data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 data = dict(
-    imgs_per_gpu=1,
+    imgs_per_gpu=4,
     workers_per_gpu=4,
     train=dict(
         type=dataset_type,
