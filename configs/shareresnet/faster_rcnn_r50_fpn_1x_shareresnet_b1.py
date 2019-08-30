e22741be502a239185c9560f2c5426bb99d09b08
劉子铭 <zimingemail@163.com>
2019-08-30 13:16:18 +0800

    add shallow feat in  n  stage

--- configs/shareresnet/faster_rcnn_r50_fpn_1x_shareresnet_b1.py
+++ configs/shareresnet/faster_rcnn_r50_fpn_1x_shareresnet_b1.py
@@ -172,7 +172,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_r50_fpn_1x_shareresnet'
+work_dir = './work_dirs/faster_rcnn_r50_fpn_1x_shareresnet_b1'
 load_from = None
 resume_from = None #'./work_dirs/faster_rcnn_r50_fpn_1x_shareresnet/epoch_5.pth'
 workflow = [('train', 1)]
